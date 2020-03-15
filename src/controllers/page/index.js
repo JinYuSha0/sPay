7abb7b5d765d1c7758945838dda774921b5200b8
JinYuSha0 <a1009943858@gmail.com>
2020-03-15 23:13:40 +0800

    更新根路径

--- src/controllers/page/index.js
+++ src/controllers/page/index.js
@@ -2,7 +2,7 @@ const Router = require('koa-router')
 
 const PageRouter = new Router()
 
-PageRouter.get('/pay', require('./pay'))
+PageRouter.get('/', require('./pay'))
 PageRouter.get('/process', require('./process'))
 
 module.exports = PageRouter
